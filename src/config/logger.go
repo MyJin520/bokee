87ef59c816b2565a1191cddf636af55af59dec2b
JinYu <3138641158@qq.com>
2026-05-30 20:08:43 +0800

    imp: 删除冗余注释

--- src/config/logger.go
+++ src/config/logger.go
@@ -1,12 +1,12 @@
 package config
 
 type LogConfig struct {
-	Level      string `yaml:"level"`       // 日志级别 debug/info/warn/error
-	Format     string `yaml:"format"`      // 输出格式 json/console
-	Output     string `yaml:"output"`      // 输出路径 stdout/file
-	FilePath   string `yaml:"file-path"`   // 文件路径
-	MaxSize    int    `yaml:"max-size"`    // 单个日志文件最大大小(MB)
-	MaxBackups int    `yaml:"max-backups"` // 保留的旧日志文件最大数量
-	MaxAge     int    `yaml:"max-age"`     // 保留旧日志文件的最大天数
-	Compress   bool   `yaml:"compress"`    // 是否压缩
+	Level      string `yaml:"level"`
+	Format     string `yaml:"format"`
+	Output     string `yaml:"output"`
+	FilePath   string `yaml:"file-path"`
+	MaxSize    int    `yaml:"max-size"`
+	MaxBackups int    `yaml:"max-backups"`
+	MaxAge     int    `yaml:"max-age"`
+	Compress   bool   `yaml:"compress"`
 }
--- src/config/system.go
+++ src/config/system.go
@@ -1,7 +1,7 @@
 package config
 
 type SystemConfig struct {
-	DbType  string `yaml:"db-type"`  // 数据库类型 mysql/postgres
-	OssType string `yaml:"oss-type"` // 存储类型
-	Addr    string `yaml:"addr"`     // 服务地址
+	DbType  string `yaml:"db-type"`
+	OssType string `yaml:"oss-type"`
+	Addr    string `yaml:"addr"`
 }
