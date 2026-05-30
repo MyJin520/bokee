e7399b2b446e9f0d3e0475b8d545db34a4090acd
JinYu <3138641158@qq.com>
2026-05-30 20:06:38 +0800

    imp: 为项目添加迁移表功能

--- src/initialize/db.go
+++ src/initialize/db.go
@@ -2,6 +2,8 @@ package initialize
 
 import (
 	"gin-admin/config"
+	"gin-admin/global"
+	"gin-admin/internal/mods/basic"
 	db "gin-admin/pkg/gormx"
 )
 
@@ -9,5 +11,18 @@ func initDb(cfg *config.Config) {
 	err := db.InitDB(cfg)
 	if err != nil {
 		panic("数据库初始化失败: " + err.Error())
+	}
+	migrateTable()
+}
+
+// 迁移表结构
+func migrateTable() {
+	err := global.DB.AutoMigrate(
+		// 基础模型
+		&basic.User{},
+		&basic.Role{},
+	)
+	if err != nil {
+		panic("数据库迁移失败: " + err.Error())
 	}
 }
