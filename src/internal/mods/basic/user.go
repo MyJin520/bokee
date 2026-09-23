c1d6f81f340492f519681b43bcb7a469ef1cab18
JinYu <3138641158@qq.com>
2026-09-23 10:55:38 +0800

    ref: 格式化表结构体

--- src/internal/mods/basic/user.go
+++ src/internal/mods/basic/user.go
@@ -8,6 +8,5 @@ type User struct {
 	Email    string `gorm:"size:64;comment:邮箱"`
 	Status   string `gorm:"size:10;not null;default:normal;comment:账号状态"`
 	Avatar   string `gorm:"size:255;comment:头像地址"`
-
-	Roles []Role `gorm:"many2many:sys_user_roles;constraint:OnDelete:CASCADE"`
+	Roles    []Role `gorm:"many2many:sys_user_roles;constraint:OnDelete:CASCADE"`
 }
